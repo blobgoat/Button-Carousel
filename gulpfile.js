cd4992bbe7ab38bafccfe0959994cdf56d021241
blobgoat <jacobaaronseaman@gmail.com>
2026-07-08 14:11:16 -0700

    fixed gulp file and made better build workflow

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import autoprefixer from 'gulp-autoprefixer';
 
 gulp.task('styles', function () {
-    return gulp.src('src/style.css')
+    return gulp.src('dist/index.css')
         .pipe(autoprefixer())
         .pipe(gulp.dest('dist'));
 });
